b86e823905d64ff3bc24d8ffea874e7abe8c9fe6
Asharp19 <rishabh.3172000@gmail.com>
2024-04-24 12:06:15 +0300

    Foundry Update NFTMaker

--- src/NFTMaker.sol
+++ src/NFTMaker.sol
@@ -1,33 +1,29 @@
-// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >=0.8.2 <0.9.0;
-
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@ERC721A/contracts/ERC721A.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-
 contract SingularNFTMaker is ERC721URIStorage, Ownable {
     address creator = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-    uint256 public constant MINT_PRICE = 1 ether;
-    mapping(address => bool) private approvedAddresses;
+    uint256 public constant MINT_PRICE = 1 gwei;
+    mapping(address => uint8) private approvedAddresses;
     mapping(address whiteListedAddress => uint8 index) private rarityIndex;
+    mapping(uint256 tokenId => uint256 tokenPrice) private pricesOfNfts;
     uint8[] private rarity;
     string[] private nftURIs;
     uint8[] private rangedRarity;
     uint256 private lastTokenId = 1;
 
-    constructor(string memory nftName, string memory nftSymbol, uint8[] memory _rarity, string[] memory _nftURIs)
-        payable
-        ERC721(nftName, nftSymbol)
-        Ownable(msg.sender)
-    {
+    constructor(
+        string memory nftName,
+        string memory nftSymbol,
+        uint8[] memory _rarity,
+        string[] memory _nftURIs
+    ) payable ERC721(nftName, nftSymbol) Ownable(msg.sender) {
         if (msg.value < MINT_PRICE) {
             payable(msg.sender).transfer(msg.value);
             assert(false);
         }
 
-        require(rarity.length == nftURIs.length, "Rarity and nftURI need to have the same length");
+        require(
+            rarity.length == nftURIs.length,
+            "Rarity and nftURI need to have the same length"
+        );
         uint8 sum = 0;
         for (uint256 i = 0; i < _rarity.length; i++) {
             require(_rarity[i] > 0, "Rarity values must be greator than zero");
@@ -35,7 +31,13 @@ contract SingularNFTMaker is ERC721URIStorage, Ownable {
             rangedRarity.push(sum);
         }
 
-        require(sum == uint8(100), string.concat("Sum of rarities must equal 100, got:", Strings.toString(sum)));
+        require(
+            sum == uint8(100),
+            string.concat(
+                "Sum of rarities must equal 100, got:",
+                Strings.toString(sum)
+            )
+        );
 
         require(payable(creator).send(MINT_PRICE), "Failed to transfer funds");
         rarity = _rarity;
@@ -51,22 +53,35 @@ contract SingularNFTMaker is ERC721URIStorage, Ownable {
     function whiteListAddresses(address[] memory addresses) public onlyOwner {
         for (uint256 index = 0; index < addresses.length; index++) {
             if (addresses[index] != address(0)) {
-                approvedAddresses[addresses[index]] = true;
-                rarityIndex[addresses[index]] = getRarityIndex(generateRandgedRarityIndex(addresses[index]));
+                approvedAddresses[addresses[index]] = approvedAddresses[addresses[index]]+1;
+                rarityIndex[addresses[index]] = getRarityIndex(
+                    generateRandgedRarityIndex(addresses[index])
+                );
             }
         }
     }
 
-    function mintNew(address nftOwner) public payable returns (bool) {
-        require(approvedAddresses[nftOwner], "You have not been approved as an owner");
+    function isApproved(address nftOwner) public view returns (bool) {
+        return approvedAddresses[nftOwner]>0;
+    }
 
-        require(payable(owner()).send(getMintPrice(nftOwner)), "Failed to transfer required funds");
+    function mintNew(address nftOwner) public payable returns (bool) {
+        require(
+            approvedAddresses[nftOwner]>0,
+            "You have not been approved as an owner"
+        );
+        console.log(msg.sender);
+        console.log(owner());
+        require(
+            payable(owner()).send(getMintPrice(nftOwner)),
+            "Failed to transfer required funds"
+        );
         uint256 currentTokenId = lastTokenId + 1;
         uint8 nftRarityIndex = rarityIndex[nftOwner];
         _mint(nftOwner, currentTokenId);
         _setTokenURI(currentTokenId, nftURIs[nftRarityIndex]);
         lastTokenId = currentTokenId;
-        approvedAddresses[nftOwner] = false;
+        approvedAddresses[nftOwner] = approvedAddresses[nftOwner]-1;
         delete rarityIndex[nftOwner];
         return true;
     }
@@ -75,12 +90,18 @@ contract SingularNFTMaker is ERC721URIStorage, Ownable {
         return MINT_PRICE / rarityIndex[nftOwner];
     }
 
-    function generateRandgedRarityIndex(address addr) public view returns (uint8) {
-        uint256 rand = uint256(keccak256(abi.encodePacked(block.timestamp, addr, block.difficulty))) % 100;
+    function generateRandgedRarityIndex(
+        address addr
+    ) private view returns (uint8) {
+        uint256 rand = uint256(
+            keccak256(abi.encodePacked(block.timestamp, addr, block.difficulty))
+        ) % 100;
         return uint8(rand + 1);
     }
 
-    function getRarityIndex(uint8 _rangedIndex) private returns (uint8 _rarityIndex) {
+    function getRarityIndex(
+        uint8 _rangedIndex
+    ) private returns (uint8 _rarityIndex) {
         require(_rangedIndex > 0, "Randged Index must be greater than zero");
         for (uint256 i = 0; i < rangedRarity.length; i++) {
             if (_rangedIndex <= rangedRarity[i]) {
@@ -89,4 +110,25 @@ contract SingularNFTMaker is ERC721URIStorage, Ownable {
         }
         return uint8(rangedRarity.length - 1);
     }
+
+    function setNftPrice(uint256 price,uint256 tokenId)public view returns (bool){
+        require(_ownerOf(tokenId)==msg.sender,"You are not the owner of the NFT");
+        pricesOfNfts[tokenId] = price;
+        return true;
+    }
+
+    function getNftPrice(uint256 tokenId)public view returns(uint256){
+        require(pricesOfNfts[tokenId]>0,"Nft Not for sale");
+        return pricesOfNfts[tokenId];
+    }
+
+    function buyNft(uint256 tokenId) public payable returns(bool) {
+        uint256 priceofNft = pricesOfNfts[tokenId];
+        require(priceofNft > 0, "Nft Not for sale");
+        require(msg.value < priceofNft, "Invalid Price");
+        _transfer(ownerOf(tokenId), msg.sender, tokenId);
+        payable(seller).transfer(msg.value); 
+        pricesOfNfts[tokenId] = 0; 
+        return true;
+     
 }
--- test/SingularNftMaker.t.sol
+++ test/SingularNftMaker.t.sol
@@ -5,13 +5,11 @@ import "../src/NFTMaker.sol";
 import {Test, console} from "forge-std/Test.sol";
 
 contract TestNFTMaker is Test {
+    SingularNFTMaker public signupContract;
+
     function setUp() public {
         // interfaceMain=IUserSignup(address(signUpContract));
         // vm.startPrank(0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496);
-        // console.log(address(signUpContract));
-    }
-
-    function testWhiteList() public {
         uint8[] memory rarityArray = new uint8[](7);
         rarityArray[0] = 2;
         rarityArray[1] = 3;
@@ -29,19 +27,26 @@ contract TestNFTMaker is Test {
         uriArray[4] = "xyz.com";
         uriArray[5] = "xyz.com";
         uriArray[6] = "xyz.com";
-        // console.log(balanceOf(msg.sender));
-        address owner = msg.sender;
 
         vm.startPrank(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
         vm.deal(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, 10 ether);
+
+        signupContract=new SingularNFTMaker{value: 1 ether}("NFT2", "N1", rarityArray, uriArray);
+        // console.log(address(signUpContract));
+    }
+
+    function testWhiteList() public {
+        
+        // console.log(balanceOf(msg.sender));
+        // address owner = msg.sender;
+
+        
         // console.log(owner.balance);
-        SingularNFTMaker signUpContract = new SingularNFTMaker{value: 1 ether}("NFT2", "N1", rarityArray, uriArray);
+        // SingularNFTMaker signUpContract = new SingularNFTMaker{value: 1 ether}("NFT2", "N1", rarityArray, uriArray);
         address[] memory whiteListArray = new address[](2);
         whiteListArray[0] = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
         whiteListArray[1] = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
 
-        // console.log(signUpContract.owner());
-        // mainContract.whiteListAddresses(whiteListArray);
 
         signUpContract.whiteListAddresses(whiteListArray);
         vm.startPrank(0x90F79bf6EB2c4f870365E785982E1f101E93b906);
@@ -51,7 +56,7 @@ contract TestNFTMaker is Test {
         assert(success);
     }
 
-    // function invariantWhitelistRemoval() public {
-
-    // }
+    function invariantWhitelistRemoval() public {
+        
+    }
 }
